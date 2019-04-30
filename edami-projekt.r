dce5a57d441860bf2be360e4d72009c2b1dcf3ac
Maciej Purski <maciej.purski@gmail.com>
2019-04-30 18:40:06 +0200

    BUILD phase working

--- edami-projekt.r
+++ edami-projekt.r
@@ -1,11 +1,10 @@
 library(cluster)
 
-
 data("USArrests")
 df <- scale(USArrests)
 head(df, n = 3)
 
-pam.res <- pam(df, 3, do.swap = FALSE)
+pam.res <- pam(df, 8, do.swap = FALSE, trace.lev=10, stand=FALSE)
 print(pam.res)
 str(pam.res)
 View(df)
@@ -13,47 +12,51 @@ View(df)
 
 # my pam implementation
 
-k <- 3
+k <- 8
 euc.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
 
 # Function to compute dissimilarity of an object x
 # to the closest object in S
-dissToClosest <- function(x, S, d) {
-  min(unlist(lapply(S, function(y) { euc.dist(d[x, ], d[y, ]) })))
+dissToClosest <- function(x, C, d) {
+  min(unlist(lapply(C, function(y) euc.dist(d[x, ], d[y, ]) )))
 }
 
-computeC <- function(i, j, S, d) {
-  max(dissToClosest(j, S, d) - euc.dist(d[i,], d[j,]), 0)
+computeC <- function(i, j, C, d) {
+  min(dissToClosest(j, C, d), euc.dist(d[i,], d[j,]))
+}
+
+distToMedoids <- function(j, C, d) {
+  sapply(C, function(n) euc.dist(d[j, ], d[n, ]))
 }
 
 # BUILD STEP
-# U is a list of indices of unselected rows
-# S is a list of indices of selected rows, e.t rows which are centers
-U <- 1:nrow(df)
-S <- list()
+# O is a list of indices of unselected rows
+# S is a list of indices of selected rows - clusters
+O <- 1:nrow(df)
+C <- list()
 
 
-sumOfDistances <- lapply(U,
-                  function(i) Reduce("+", lapply(U,
+sumOfDistances <- lapply(O,
+                  function(i) Reduce("+", lapply(O,
                   function(j) euc.dist(df[i,], df[j,]))))
 
-# S - selected
-S <- append(S, which.min(sumOfDistances))
+# C - selected
+C <- append(C, which.min(sumOfDistances))
 
-# U - unselected
-U <- U[!U %in% S]
+# O - unselected
+O <- O[-unlist(C)]
 
 # Compute matrix C
-while (length(S) != k) {
-  C <- lapply(U, function(i) lapply(U[!U %in% i],
-               function(j) computeC(i, j, S, df)))
+while (length(C) != k) {
+  M <- lapply(O, function(i) lapply(O[!O %in% i],
+                                    function(j) computeC(i, j, C, df)))
 
   # find best candidate to add to set S, s is the index in U list
-  s <- which.max(lapply(C, function(x) Reduce("+", x)))
-  S <- append(S, U[s])
-  U <- U[-s]
+
+  s <- which.min(lapply(M, function(x) Reduce("+", x)))
+  C <- append(C, O[s])
+  O <- O[-s]
 }
 
-unlist(S)
-df[unlist(S),]
-pam.res$medoids
+dists <- lapply(O, function(j) sort(distToMedoids(j, C, df)))
+View(dists)
